f1dd669956909d8629d0bd004f1d620979e8ef2f
wagmi7 <kamaleinstein1@gmail.com>
2024-03-01 11:17:14 +0530

    pinata researchers list fixed

--- descihubonbnb-frontend/src/components/researcher/ResearcherList.tsx
+++ descihubonbnb-frontend/src/components/researcher/ResearcherList.tsx
@@ -1,36 +1,39 @@
 // @ts-nocheck
-import {  useEffect } from 'react';
-import { Box, Text, Divider, Flex } from '@chakra-ui/react';
+import { useEffect, useState } from 'react';
+import { Box, Text, Divider, Flex, Image } from '@chakra-ui/react';
+import { toast } from 'react-toastify';
 
 const ResearchersList = () => {
-  
-  const pinata = new pinataSDK({ pinataJWTKey: process.env.PINATA_JWT });
-
   const [researchers, setResearchers] = useState([]);
 
   useEffect(() => {
     const fetchResearchers = async () => {
       try {
-        const response = await pinata.pinList(
-          { pinataJWTKey: process.env.PINATA_JWT },
-          {
-            pageLimit: 1,
-          }
-        );
-        res.json(response.rows[0]);
+        const res = await fetch("/api/files"); // returns the array of researchers' CIDs
+        const cids = await res.json();
+
+        const dataPromises = cids.map(async (cid) => {
+          const response = await fetch(`https://gateway.pinata.cloud/ipfs/${cid.ipfs_pin_hash}`);
+          const data = await response.json();
+          return data;
+        });
+
+        // Waiting for all fetch requests to complete
+        const researchersData = await Promise.all(dataPromises);
+        toast.success("All researchers fetched successfully");
+        setResearchers(researchersData);
       } catch (e) {
-        console.log(e);
-        res.status(500).send("Server Error");
+        console.error(e);
       }
-    }
+    };
 
     fetchResearchers();
-  }, []); 
+  }, []);
 
   return (
     <Box>
       <Text fontSize="2xl" fontWeight="bold" mb={4}>
-        Researchers List
+        Best Researchers
       </Text>
       <Flex flexWrap="wrap">
         {researchers.map((researcher, index) => (
@@ -44,11 +47,14 @@ const ResearchersList = () => {
             mb={4}
             mr={4}
           >
-            <img src={researcher.profileUrl} width={300} height={300} alt="Profile Image" />
+            {/* Assuming the researcher data includes a profileUrl field */}
+            {researcher.profileUrl && (
+              <Image src={researcher.profileUrl} boxSize="300px" alt="Profile Image" objectFit="cover" />
+            )}
             <Text fontSize="lg" fontWeight="semibold">
               Name: {researcher.name}
             </Text>
-            <Text>Wallet Address - {researcher.walletAddress}</Text>
+            <Text>Wallet Address: {researcher.walletAddress}</Text>
             <Text>Institution: {researcher.institution}</Text>
             <Divider my={2} />
           </Box>
